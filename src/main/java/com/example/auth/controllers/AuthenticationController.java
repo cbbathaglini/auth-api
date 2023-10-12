2b7073a81496e8497fa96de03ba6c9619a8966bd
Carine Bathaglini <cbbathaglini@gmail.com>
2023-10-11 23:02:29 -0300

    zerando o repositório

--- src/main/java/com/example/auth/controllers/AuthenticationController.java
+++ src/main/java/com/example/auth/controllers/AuthenticationController.java
@@ -5,7 +5,6 @@ import com.example.auth.domain.user.LoginResponseDTO;
 import com.example.auth.domain.user.RegisterDTO;
 import com.example.auth.domain.user.User;
 import com.example.auth.infra.security.TokenService;
-import com.example.auth.repositories.UserRepository;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
